87c9c2c7ec612dea9e270f891faf965d3cf21f59
Li Yuan <lyuan@gd.edu.kg>
2024-02-15 19:00:02 +0000

    version 0.2.0

--- R/available.models.R
+++ R/available.models.R
@@ -1,9 +1,9 @@
-#' Get supported Generative AI models
+#' Get Supported Generative AI Models
 #'
-#' This function sends a request to GenAI database API
-#' to retrieve information about available Generative AI models.
+#' This function sends a request to GenAI database API to retrieve information
+#' about available generative AI models.
 #'
-#' @return If successful, the function returns a list containing Generative AI
+#' @return If successful, the function returns a list containing generative AI
 #' service providers and their corresponding models. If the function encounters an error,
 #' it will halt execution and provide an error message.
 #'
@@ -13,18 +13,21 @@
 #' offered by various service providers.
 #'
 #' @seealso
-#' \href{https://genai.gd.edu.kg/r/documentation/}{GenAI - R Documentation}
+#' \href{https://genai.gd.edu.kg/r/documentation/}{GenAI - R Package "GenAI" Documentation}
+#'
+#' \href{https://colab.research.google.com/github/GitData-GA/GenAI/blob/gh-pages/r/example/available_models.ipynb}{Live Demo in Colab}
 #'
 #' @examples
 #' \dontrun{
-#'  models = available.models()
-#'  models
+#' # Assuming there is a GenAI object named 'genai.model' supporting this
+#' # function, please refer to the "Live Demo in Colab" above for real
+#' # examples. The following examples are just some basic guidelines.
+#'
+#' all.models = available.models() %>% print()
 #' }
 #'
 #' @export
-#'
-#' @importFrom jsonlite toJSON
-available.models = function () {
+available.models = function() {
   json.data = jsonlite::fromJSON("https://genai.gd.edu.kg/model.json")
   return (json.data)
 }
